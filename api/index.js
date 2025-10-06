5714bac62b205803f43a712abe57a49ff1161df8
Moisés <moiseavilar0@gmail.com>
2025-10-06 09:02:25 -0300

    Fix: Final adjustments for Vercel API routing

--- api/index.js
+++ api/index.js
@@ -2,10 +2,19 @@ const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+
+const router = jsonServer.router(path.join(__dirname, 'db.json')); 
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use((req, res, next) => {
+  if (req.url.startsWith('/api')) {
+    req.url = req.url.substring(4);
+  }
+  next();
+});
+
 server.use(router);
 
 module.exports = server;
